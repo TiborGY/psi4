fd765d93692ab762dc8f2e346b938fb50da8e96e
Tibor Győri <tibor.gyori@chem.u-szeged.hu>
2023-12-10 00:08:01 +0100

    make MOInfoBase::nso private

--- psi4/src/psi4/libmoinfo/moinfo_base.h
+++ psi4/src/psi4/libmoinfo/moinfo_base.h
@@ -117,7 +117,7 @@ class MOInfoBase {
     int wfn_sym;
     int multiplicity;
 
-    int nso;  // PSI nso (# of symmetry-adapted atomic orbitals)
+
     int nmo;  // Psi nmo (# of molecular orbitals, including frozen core and frozen virtual)
     int ndocc;
     int nactv;  // Total # of active orbitals across all irreps
@@ -142,6 +142,7 @@ class MOInfoBase {
     intvec convert_int_array_to_vector(int n, const int* array);
 
     const int charge;  // The charge this object has been constructed with
+    int nso;  // PSI nso (# of symmetry-adapted atomic orbitals)
     intvec sopi;  // Array holding the numbers of SOs per irrep
 };
 
