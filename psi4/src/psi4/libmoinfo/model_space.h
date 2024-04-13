62155f60c29458a030a4c70c9539ede6459192dd
Tibor Győri <tibor.gyori@chem.u-szeged.hu>
2024-04-13 21:46:12 +0200

    this is really strange that this shows up in the diff

--- psi4/src/psi4/libmoinfo/model_space.h
+++ psi4/src/psi4/libmoinfo/model_space.h
@@ -59,6 +59,9 @@ class ModelSpace {
     std::vector<int> unique_to_all;  // spin-unique  determinants
     MOInfo* moinfo_obj;
 };
+
+extern ModelSpace* model_space;
+
 }  // namespace psi
 
 #endif  // _psi_src_lib_libmoinfo_model_space_h_
